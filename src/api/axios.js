a77617f1be98e4f7edc479b0a67681355407fc46
M-sandoval-93 <sandoval.mario.08@gmail.com>
2023-11-21 01:08:23 -0300

    update workin in home

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  // baseURL: "http://localhost/www/api-matriculas",
-  // baseURL: "http://localhost/api-matriculas",
-  baseURL: "http://172.16.0.92/api-matriculas",
+  // baseURL: "api-matricula",
+  baseURL: "http://localhost/api-matriculas",
+  // baseURL: "http://172.16.0.92/api-matriculas",
 });
